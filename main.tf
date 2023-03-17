49ab008c1cd6b107b5f53c5f2f0354f043fee3b5
A <github982457@protonmail.com>
2023-03-17 18:02:19 +0000

    Values for first run

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    resource_group_name  = "rg-tfghactions-dev-001"
+    storage_account_name = "safri17marv1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
