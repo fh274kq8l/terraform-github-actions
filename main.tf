2c4affc516dde449db9273ac06bf18e863e01a9b
A <github982457@protonmail.com>
2023-03-18 17:27:21 +0000

    Updated with TF state

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-tfghactions-dev-001"
-    storage_account_name = "safri17marv1"
+    resource_group_name  = "rg-satfstate18032023-dev-001"
+    storage_account_name = "satfstate18032023"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
