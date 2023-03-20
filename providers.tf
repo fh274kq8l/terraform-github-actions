a8019211f178f9f4c334425cd71e06531b672a08
A <github982457@protonmail.com>
2023-03-20 11:45:03 +0000

    Guess again

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-satfstate20032023-dev-001"
+    storage_account_name = "satfstate20032023"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }   
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
